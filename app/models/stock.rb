df8dfd140db0bacd6ca3262ac77d49fcd2890e30
Gerry Tamakloe <x10209778@student.ncirl.ie>
2011-11-15 22:14:46 +0000

    StockAppGT

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,14 +3,16 @@ class Stock < ActiveRecord::Base
 
 validates_presence_of :symbol
  
-
  def validate
 	errors.add(:symbol, "Must be a valid Ticker Symbol") if date.nil?|| date == "invalid date"
   end
 
 
 
+
+
 	require 'net/http'
+	#require 'parsedate'
 
   def Stock::getStock(ticker)
 	# Special Characters:d1=date,t1=Last Trade Time, s=Symbol, n=Name, g=Day's Low, l1=Last Trade (Price Only), p2=Percent Change, h=Day's High, x=Stock Exchange (ref:http://www.gummy-stuff.org/Yahoo-data.htm)
