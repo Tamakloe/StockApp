8b569fde4222d8e2bbed2e9dc67a535143b8c200
Gerry Tamakloe <x10209778@student.ncirl.ie>
2011-11-15 21:25:45 +0000

    StockAppGT

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,12 +3,11 @@ class Stock < ActiveRecord::Base
 
 validates_presence_of :symbol
  
+
  def validate
 	errors.add(:symbol, "Must be a valid Ticker Symbol") if date.nil?|| date == "invalid date"
   end
 
-
-unloadable
 
 
 	require 'net/http'
